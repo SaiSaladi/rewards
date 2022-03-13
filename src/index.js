ec08a9593097faf9ec9bb07fb855a472d6047d3d
Sai <Sai@Sais-MacBook-Air.local>
2022-03-12 21:39:10 -0500

    wip: cleanup of unwanted files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import User from './routes/user';
 import Users from './routes/users';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
